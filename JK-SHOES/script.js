26a49115d11acd744c508af2016fe955a8265ae4
Revsowmi <Revanthtpr02@gmail.com>
2026-08-05 01:54:02 +0530

    Delete JK-SHOES directory

--- JK-SHOES/script.js
+++
@@ -1,5 +0,0 @@
-const themeButton = document.getElementById("themeButton");
-themeButton.addEventListener("click", () => {
-  document.body.classList.toggle("dark");
-});
-// This file is optional. It can stay empty for now.
